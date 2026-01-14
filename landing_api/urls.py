0601953e4a1d2744e4a483de33dbcf2b3ca35933
cgutierrez05 <carlagutierrezc05@gmail.com>
2026-01-14 19:17:20 +0000

    guia 22 corregida

--- landing_api/urls.py
+++ landing_api/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 from landing_api import views
 
 urlpatterns = [
-    path('', views.LandingAPI.as_view(), name='landing-api'),
     path('index/', views.LandingAPI.as_view(), name='landing-index'),
 ]
