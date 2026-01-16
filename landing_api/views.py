d355580f8fef0a14d7ebdb08459d1263fae81b27
Darwin Leonardo Diaz González <dldiaz@espol.edu.ec>
2026-01-16 07:15:45 +0000

    Error landing_api POST solucionado

--- landing_api/views.py
+++ landing_api/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from firebase_admin import db
-import datetime
+from datetime import datetime
 
 class LandingAPI(APIView):
     name = "Landing API"
